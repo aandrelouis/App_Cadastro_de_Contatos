33d6f695ec49e8f599c1e04297614203115c6e60
aandrelouis <aandrelouis@gmail.com>
2022-02-18 17:35:49 -0300

    impressao do cadastro

--- pages/Informacao/index.js
+++ pages/Informacao/index.js
@@ -1,5 +1,6 @@
 import { useEffect, useState } from 'react';
 import { StyleSheet, Text, View, TextInput, Button, ScrollView, TouchableOpacity , Image} from 'react-native';
+import { AsyncStorage } from 'react-native';
 
 
 export default function Informacao({navigation, route}) {
